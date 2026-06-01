8bc507a69ce84a8f57fee238e1bb1f9c8aa64fb2
yohei.yoshikawa@gmail.com <yohei.yoshikawa@gmail.com>
2026-06-02 01:31:45 +0900

    update

--- templates/11_map_api/js/map.js
+++ templates/11_map_api/js/map.js
@@ -21,6 +21,7 @@ const tokyo = { lat: 35.681236, lng: 139.767125 };
 // 地図スタイルの定義
 const mapStyles = {
   default: null,
+  // 淡い色合いのスタイル
   muted: [
     { elementType: 'geometry', stylers: [{ color: '#f5f5f5' }] },
     { elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
@@ -30,6 +31,7 @@ const mapStyles = {
     { featureType: 'water', elementType: 'geometry', stylers: [{ color: '#a5f3fc' }] },
     { featureType: 'poi.park', elementType: 'geometry', stylers: [{ color: '#bbf7d0' }] },
   ],
+  // 夜間モード風のスタイル
   night: [
     { elementType: 'geometry', stylers: [{ color: '#18181b' }] },
     { elementType: 'labels.text.fill', stylers: [{ color: '#d4d4d8' }] },
@@ -42,25 +44,32 @@ const mapStyles = {
 
 // 地図初期化関数: Google Maps API の読み込み後に呼び出される
 window.initMap = function initMap() {
-  geocoder = new google.maps.Geocoder();
+  // ジオコーディング API 
+  // 現在の座標から住所を取得
+  // geocoder = new google.maps.Geocoder();
+
+  // 情報ウィンドウ
   infoWindow = new google.maps.InfoWindow();
 
-  // 地図の初期化
+  // TODO: 地図の初期化
+  // center: tokyo 
+  // zoom: 14
+  // mapTypeId: 'roadmap'
   map = new google.maps.Map(document.getElementById('map'), {
     center: tokyo,
-    zoom: 14,
+    zoom: 1,
     mapTypeId: 'roadmap',
     mapTypeControl: false,
     streetViewControl: true,
     fullscreenControl: true,
   });
 
-  // 初期マーカーの設定
-  selectedMarker = new google.maps.Marker({
-    position: tokyo,
-    map,
-    title: '東京駅',
-  });
+  // TODO: 初期マーカーの設定
+  // selectedMarker = new google.maps.Marker({
+  //   position: tokyo,
+  //   map,
+  //   title: '東京駅',
+  // });
 
   bindEvents();
   showSelectedPoint(tokyo);
@@ -76,7 +85,8 @@ function bindEvents() {
   // 地図タイプ切替ボタンのイベント
   document.querySelectorAll('.map-type').forEach((button) => {
     button.addEventListener('click', () => {
-      setMapType(button.dataset.type);
+      // TODO: data-type 属性を使って地図のタイプを切り替える
+      // setMapType(button.dataset.type);
       setActiveButton('.map-type', button, 'bg-zinc-900');
     });
   });
@@ -84,6 +94,7 @@ function bindEvents() {
   // 描画モード切替ボタンのイベント
   document.querySelectorAll('.draw-mode').forEach((button) => {
     button.addEventListener('click', () => {
+      // TODO: data-mode 属性を使って描画モードを切り替える
       activeMode = button.dataset.mode;
       setActiveButton('.draw-mode', button, 'bg-cyan-700');
       updateModeMessage();
@@ -101,8 +112,8 @@ function bindEvents() {
 
 // 地図クリック時の処理
 function handleMapClick(latLng) {
-  // マーカーモード
-  if (activeMode === 'marker') {
+  // TODO: マーカーモード: marker
+  if (activeMode === '') {
     const position = latLng.toJSON();
     selectedMarker.setPosition(position);
     map.panTo(position);
@@ -110,20 +121,22 @@ function handleMapClick(latLng) {
     return;
   }
 
-  // 線描画モード
-  if (activeMode === 'line') {
+  // TODO: 線描画モード: line
+  if (activeMode === '') {
     addLinePoint(latLng);
     return;
   }
 
-  // 範囲描画モード
-  if (activeMode === 'polygon') {
+  // TODO: 範囲描画モード: polygon
+  if (activeMode === '') {
     addPolygonPoint(latLng);
     return;
   }
 
-  // 円描画モード
-  drawCircle(latLng);
+  // TODO: 円描画モード: circle
+  if (activeMode === 'circle') {
+    drawCircle(latLng);
+  }
 }
 
 // 地図タイプの切替
@@ -216,12 +229,12 @@ function drawCircle(latLng) {
 
 // クリックした地点にマーカーを追加
 function addClickMarker(latLng, label) {
+  // クリックマーカーを追加: google.maps.Marker
   const marker = new google.maps.Marker({
     position: latLng,
     map,
     label,
   });
-
   clickMarkers.push(marker);
 }
 
